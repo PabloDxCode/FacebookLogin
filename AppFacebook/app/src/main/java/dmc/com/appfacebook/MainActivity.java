4c3a3ba17847f0ba3e1f9625edf0c80ed58909f2
Pablo Gutiérrez Díaz <dxpablo2012@hotmail.com>
2016-11-09 10:05:28 -0600

    Ultimos cambios
    sin comentarios

--- AppFacebook/app/src/main/java/dmc/com/appfacebook/MainActivity.java
+++ AppFacebook/app/src/main/java/dmc/com/appfacebook/MainActivity.java
@@ -2,29 +2,44 @@ package dmc.com.appfacebook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
     private LoginButton loginButton;
-    CallbackManager callbackManager;
+    private TextView info;
+    private CallbackManager callbackManager;
+
+    private String email,name,first_name,last_name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
         FacebookSdk.sdkInitialize(getApplicationContext());
         callbackManager = CallbackManager.Factory.create();
         //AppEventsLogger.activateApp(this);
 
+        setContentView(R.layout.activity_main);
+
+        info = (TextView)findViewById(R.id.info);
         loginButton = (LoginButton) findViewById(R.id.login_button);
         loginButton.setReadPermissions("email");
 
@@ -32,16 +47,50 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onSuccess(LoginResult loginResult) {
                 // App code
+                GraphRequest graphRequest   =   GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback()
+                {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response)
+                    {
+                        Log.d("JSON", "" + response.getJSONObject().toString());
+
+                        try
+                        {
+                            email       =   object.getString("email");
+                            name        =   object.getString("name");
+                            first_name  =   object.optString("first_name");
+                            last_name   =   object.optString("last_name");
+                        }
+                        catch (JSONException e)
+                        {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+
+                info.setText(
+                        "User ID: "
+                                + loginResult.getAccessToken().getUserId()
+                                + "\n" +
+                                "Auth Token: "
+                                + loginResult.getAccessToken().getToken()
+                                +"\nEmail " + email
+                                +"\nNombre " + name
+                                +"\n Primer nombre " + first_name
+                                +"\nApellido " + last_name
+                );
             }
 
             @Override
             public void onCancel() {
                 // App code
+                info.setText("Login attempt canceled.");
             }
 
             @Override
             public void onError(FacebookException exception) {
                 // App code
+                info.setText("Login attempt failed.");
             }
         });
 
